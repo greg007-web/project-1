33f85edd13d8df180dcbd5f9af01c1ca40baa54d
greg007-web <73542972+greg007-web@users.noreply.github.com>
2023-09-26 15:46:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,44 @@
-import os
 from kivy.app import App
-from kivy.uix.camera import Camera
-from kivy.uix.label import Label
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.camera import Camera
+from kivy.uix.button import Button
+from kivy.utils import platform
+from android.permissions import request_permissions, Permission
 
 class CameraApp(App):
     def build(self):
+        # Verificar a plataforma
+        if platform == 'android':
+            self.request_android_permissions()
+
         layout = BoxLayout(orientation='vertical')
 
-        # Create camera instance
+        # Criar instância da câmera
         self.cam = Camera(resolution=(640, 480), play=True)
 
-        # Create button
-        self.btn = Button(
+        # Criar botão para capturar
+        self.capture_btn = Button(
             text="Capture",
             size_hint=(1, 0.2),
             font_size=35,
             background_color='green',
-            on_press=self.capture_image)
+            on_press=self.capture_image
+        )
 
-        # Create label
-        self.lbl = Label(
-            text="Press the button to capture",
-            size_hint=(1, 0.2))
-
-        # Add widgets to layout
         layout.add_widget(self.cam)
-        layout.add_widget(self.btn)
-        layout.add_widget(self.lbl)
-
+        layout.add_widget(self.capture_btn)
         return layout
 
+    def request_android_permissions(self):
+        permissions = [
+            Permission.CAMERA,
+            Permission.WRITE_EXTERNAL_STORAGE
+        ]
+        request_permissions(permissions)
+
     def capture_image(self, *args):
-        # Save captured image
-        self.cam.export_to_png(os.path.join(os.getcwd(), 'img.png'))
-        self.lbl.text = "Image saved as img.png"
+        # Salvar a imagem capturada
+        self.cam.export_to_png('captured_image.png')
 
 if __name__ == '__main__':
     CameraApp().run()
